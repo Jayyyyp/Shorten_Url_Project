04ef6bc8796d9a97bd30f80d59b21b9187016de4
Jayyyyp <pjae9726@gmail.com>
2023-07-14 20:53:22 +0900

    push

--- src/main/java/kr/gamso/config/EmailConfig.java
+++ src/main/java/kr/gamso/config/EmailConfig.java
@@ -23,6 +23,7 @@ public class EmailConfig {
 
     @Bean
     public JavaMailSender javaMailService() {
+        System.out.println(id);
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
 
         javaMailSender.setHost(host); // smtp 서버 주소
